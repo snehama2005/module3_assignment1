a6fabe46c366eeb1a2b48f6ae9c0194985a2074d
snehama2005 <148319920+snehama2005@users.noreply.github.com>
2023-12-06 12:22:36 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,11 +1,29 @@
-from django.http import HttpResponse
-from django.template import loader
+# from django.shortcuts import render
+# from django.views.generic.edit import CreateView
+# from.models import GreekModel
 
-# def members(request):
-#   template = loader.get_template('myfirst.html')
-#   return HttpResponse(template.render())
-# Create your views here.
+# class GreekCreate(CreateView):
+#     model=GreekModel
+#     fields=['title','description']
+#     template_name='geekmodel_form.html'
 
-def members(request):
-  template = loader.get_template('form.html')
-  return HttpResponse(template.render())
+# # Create your views here.
+from django.db import models
+
+# Create your models here.
+# from django.shortcuts import render
+# from django.views.generic.edit import CreateView
+# from.models import LoginModel
+
+# class LoginCreate(CreateView):
+    # model=LoginModel
+    # fields=["username","password"]
+    # template_name="loginmodel_form.html"
+from django.views.generic.list import ListView
+from .models import LoginModel
+class LoginList(ListView):
+    model=LoginModel
+    template_name='loginmodel_list.html'
+    context_object_name='login_list'
+    
+
